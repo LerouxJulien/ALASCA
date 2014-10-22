79ee8d32413f65cb24baf1114df62e1351c5c35f
LerouxJulien <datara@hotmail.fr>
2014-10-22 07:00:10 +0200

    Mise a jour du dispatcher
    Réécriture du répartiteur et changements dans le dispatcher.

--- fr/upmc/alasca/dispatcher/Dispatcher.java
+++ fr/upmc/alasca/dispatcher/Dispatcher.java
@@ -1,10 +1,14 @@
 package fr.upmc.alasca.dispatcher;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import fr.upmc.alasca.computer.components.VirtualMachine;
+import fr.upmc.alasca.controleurAdmission.components.Controleur;
 import fr.upmc.alasca.requestgen.objects.Request;
+import fr.upmc.components.ports.PortI;
 
 /* Classe représentant le composant interne du dispatcher
  * délèguant une application au répartiteur correspondant 
@@ -15,32 +19,35 @@ public class Dispatcher {
 	
 	/*controleur d'admission dont dépend le Dispatcher.*/
 	
-	private ControleurAdmission control;
+	private Controleur control;
 	
 	//Liste des VM provenant du controleur d'admissions **VM bouvhon utilisé**
 	
-	private ArrayList<VM> VMList;
+	private ArrayList<String> VMList;
 	
 	// Map des couples applications/repartiteurs.
 	
 	private Map<Integer ,Repartitor > repartitorList;
 	
 	
-	public Dispatcher(ControleurAdmission c){
+	public Dispatcher(Controleur c){
 		
 		control = c;
 		this.repartitorList = new HashMap<Integer,Repartitor>();
 		
 	}
 	
-	public void sendApplication(Request req,ArrayList<VM> listVM){
+	public void sendApplication(Request req,ArrayList<String> listVM){
 		
 		VMList = listVM;
-		ArrayList<VM> sendingList = new ArrayList<VM>();
+		ArrayList<String> sendingList = new ArrayList<String>();
 		
 		for(int i =0; i<VMList.size();i++){
+			PortI port = control.findPortFromURI(this.VMList.get(i));
+			VirtualMachine VM = (VirtualMachine) port.getOwner();
 			
-			if (this.VMList.get(i).getAppID()==req.getAppId() || this.VMList.get(i).getAppID()==0){
+			
+			if (VM.getAppID()==req.getAppId() || VM.getAppID()==0){
 				
 				sendingList.add(this.VMList.get(i));
 				
@@ -55,14 +62,14 @@ public class Dispatcher {
 			repart= repartitorList.get(req.getAppId());
 		}else{
 			
-			repartitorList.put(req.getAppId(), new Repartitor());
+			repartitorList.put(req.getAppId(), new Repartitor(control));
 			repart= repartitorList.get(req.getAppId());
 		}
 		 
 		int VMtoLaunch =repart.dispatch(req,sendingList);
 		if(VMtoLaunch!=0){
 			
-			ArrayList<VM> newListVM = control.requestDeploy(VMtoLaunch);
+			ArrayList<String> newListVM = control.requestDeploy(VMtoLaunch);
 			this.sendApplication(req, newListVM);
 		}
 	}
--- fr/upmc/alasca/dispatcher/Repartitor.java
+++ fr/upmc/alasca/dispatcher/Repartitor.java
@@ -1,16 +1,20 @@
 package fr.upmc.alasca.dispatcher;
 
+import java.net.URI;
 import java.util.ArrayList;
 
+import fr.upmc.alasca.computer.components.VirtualMachine;
+import fr.upmc.alasca.controleurAdmission.components.Controleur;
 import fr.upmc.alasca.requestgen.objects.Request;
+import fr.upmc.components.ports.PortI;
 
 public class Repartitor {
 
-	private static long VMMAXINST = 20000000;
+	//private static long VMMAXINST = 20000000;
+	Controleur control;
 	
-	
-	public Repartitor(){
-		
+	public Repartitor(Controleur c){
+		control = c;
 		
 	}
 	
@@ -19,85 +23,25 @@ public class Repartitor {
 	 * renvoi 0 si auvune créaion de VM n'est requise
 	 * un entier représentant le nombre a creer sinon
 	 */
-	public int dispatch(Request req, ArrayList<VM> sendingList) {
+	public int dispatch(Request req, ArrayList<String> sendingList) throws Exception {
+		for(int i =0; i<sendingList.size();i++){
+			PortI port = control.findPortFromURI(sendingList.get(i));
+			VirtualMachine VM = (VirtualMachine) port.getOwner();
 		
-		/* Etablie le nombre de vm requises minimum pour le traitement*/
-		
-		int nbVMneeded = 0;
-		long temp = req.getInstructions();
-		while (temp>VMMAXINST){
-			
-			temp -= VMMAXINST;
-			nbVMneeded ++;
-		}
-		nbVMneeded ++;
-		
-		/*Etablie la liste des VM utilisées par l'application ou libre qui peucent etre utilisées*/
-		
-		int nbVMavailable = 0;
-		ArrayList<VM> readyVMlist = new ArrayList<VM>();
+			if(!VM.queueIsFull()){
 				
-		for(int i = 0;i<sendingList.size();i++){
-			
-			VM currentVM = sendingList.get(i);
-			
-			if(!currentVM.QueuIsFull() || currentVM.appID==0){
-				
-				nbVMavailable++;
-				readyVMlist.add(currentVM);
-				
+				VM.addRequest(req);
+				return 0;
 			}
-			
 		}
-				
-		
-		/* Comparaison du nombre requis contre nombre disponible*/
-		
-		System.out.println("Etat VM, needed: "+ nbVMneeded + " / available :" + nbVMavailable);
-		//Cas où il n'y en a pas assez => retour du nombre a demarer
-		if(nbVMneeded > nbVMavailable){
-			
-			return nbVMneeded - nbVMavailable;
-			
+		return 1;
 		}
+	
 		
-		//Cas où il y en a assez => déploiement des requettes sur les VM
-		else{
-			/*Découpe des requettes */
-			long tempInst = req.getInstructions();
-			ArrayList<Request> listReq = new ArrayList<Request>();
-			
-			
-			while (tempInst!=0){
-				
-				while(tempInst>this.VMMAXINST){
-					
-					listReq.add(new Request(req.getURI(), this.VMMAXINST, req.getAppId()));
-					tempInst-=VMMAXINST;
-					System.out.println("découpe des requettes " + tempInst);
-				}
-				
-				listReq.add(new Request(req.getURI(), tempInst, req.getAppId()));
-				tempInst=0;
-				
-			}
-			
-			/* déploiement */
-			
-			for(int j = 0; j<listReq.size();j++){
-				
-				readyVMlist.get(j).deploy(listReq.get(j));
-				
-			}
-			
-			
-		}
-		
-		return 0;
 		
 		
 	}
 
 	
 
-}
+
